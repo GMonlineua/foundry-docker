76f373aff66dfeff222b8f63badef6b447fcda38
GMonline <gmonineua@gmail.com>
2023-02-23 12:15:44 +0200

    chmod u+x

--- setup.sh
+++ setup.sh
@@ -13,8 +13,6 @@ sudo passwd foundrydata
 
 # Foundry certificate
 sudo mkdir $path/ssl
-sudo mkdir $path/Config
-sudo wget https://raw.githubusercontent.com/GMonlineua/foundry-docker/master/options.json
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $path/ssl/foundry-selfsigned.key -out $path/ssl/foundry-selfsigned.crt
 
 #Foundry docker
@@ -25,9 +23,10 @@ wget -O foundryvtt.zip "$flink"
 unzip foundryvtt.zip -d ~/docker-fvtt/foundryvtt
 rm foundryvtt.zip
 
-sudo wget https://raw.githubusercontent.com/GMonlineua/foundry-docker/master/Dockerfile -o ~/docker-fvtt/Dockerfile
-sudo docker build -t foundryvtt ~/docker-fvtt/
-sudo docker run -d --name foundry -p 443:443 -v $path:/home/node -it foundryvtt:latest
+sudo curl https://raw.githubusercontent.com/GMonlineua/foundry-docker/master/Dockerfile -o ~/docker-fvtt/Dockerfile
+docker build -t foundryvtt ~/docker-fvtt/
+sudo curl https://raw.githubusercontent.com/GMonlineua/foundry-docker/master/options.json -o $path/Config/options.json
+docker run -d --name foundry -p 443:443 -v $path:/home/node -it foundryvtt:latest
 
 sudo chown -R foundrydata $path
 
